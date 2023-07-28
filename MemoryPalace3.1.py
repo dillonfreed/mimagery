27fda2e753a9e554394d9314dfa963ed3344a849
dillonfreed <42899695+dillonfreed@users.noreply.github.com>
2023-07-27 20:42:55 -0400

    Update MemoryPalace3.1.py

--- MemoryPalace3.1.py
+++ MemoryPalace3.1.py
@@ -1,5 +1,14 @@
 # Bring in deps
+import os 
 
+import streamlit as st 
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain, SequentialChain 
+from langchain.memory import ConversationBufferMemory
+from langchain.utilities import WikipediaAPIWrapper 
+
+os.environ['OPENAI_API_KEY'] = apikey
 
 # App framework
 st.title('🖤MEMORY PALACE CREATOR')
