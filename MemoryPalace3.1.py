b0d2be7d93c8eb24164c6c4644a3fc43a3b6cdc0
dillonfreed <42899695+dillonfreed@users.noreply.github.com>
2023-08-07 00:18:10 -0400

    Update MemoryPalace3.1.py

--- MemoryPalace3.1.py
+++ MemoryPalace3.1.py
@@ -7,8 +7,6 @@ from langchain.chains import LLMChain, SequentialChain
 from langchain.memory import ConversationBufferMemory
 from langchain.utilities import WikipediaAPIWrapper 
 
-os.environ['OPENAI_API_KEY'] = apikey
-
 
 # App framework
 
