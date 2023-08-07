fc445ac839b3dd6add1da528086b6cf7305bd74c
dillonfreed <42899695+dillonfreed@users.noreply.github.com>
2023-08-07 00:43:56 -0400

    Update MemoryPalace3.1.py

--- MemoryPalace3.1.py
+++ MemoryPalace3.1.py
@@ -8,7 +8,7 @@ from langchain.chains import LLMChain, SequentialChain
 from langchain.memory import ConversationBufferMemory
 from langchain.utilities import WikipediaAPIWrapper 
 
-st.write(st.secrets["apikey"])
+st.secrets.key
 # App framework
 
 st.title('🖤MEMORY PALACE CREATOR')
