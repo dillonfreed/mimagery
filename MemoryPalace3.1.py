23cc5c2bf9d5602067272e62baf09fb29f0da2f6
dillonfreed <42899695+dillonfreed@users.noreply.github.com>
2023-08-07 00:34:42 -0400

    Update MemoryPalace3.1.py

--- MemoryPalace3.1.py
+++ MemoryPalace3.1.py
@@ -16,34 +16,12 @@ prompt = st.text_input('Create your memory palace, list the furntire in your roo
 # Prompt templates
 title_template = PromptTemplate(
     input_variables = ['topic'],  
-    template='"""You are a master at hleping people remember things by using the Method of Loci \
-        Users will input a list of the furntire they have in their room in order \
-            So your first task is to create a numebered list for those user inputs \
-        which again is a list of the furniture items for the user input \
-        we will cal this ListA \
-        The user will also input thta which they wish to learn \
-        The user may input anything that they wish to learn \
-        You should generate a numbered outline of that which they wish to learn \
-       This will be called ListB \
-        so you need to make sure you produce two lists - to reiterate the first list is the items of the user inputs \
-                the second list will of the topic that the user wishes to learn that you create \
-        Next, make a third list which we will call ListC will be crated by you \
-             ListC will provide a symbol for each item of ListB only\
-        that is, you will create a relavant image that would is symbolic would immediately recognize as an assocation for each item in ListB \
-        no symbol on List C may be used more than once \
-        items on ListA are presented in counterclockwise fashion \
-                   Here are some examples of images: for Russia: Bear; Hydrogen: Zeppelin blimp; NYC: A Big Apple; etc. \
-                    these symbols will be used to create a Method of Loci story {topic}'
+    template='"""You are a master at hleping people remember things by using the Method of Loci  {topic}'
 )
 
 script_template = PromptTemplate(
     input_variables = ['title'], 
-    template='Now, create a "method of loci"story with the ListA and ListC that you created \
-    combing the symbols with the furnitue in a unique and memorable way \
-    assume all the furniture is in one room \
-    an example would be the symbol you created is using the furniture in some odd way \
-        in other words, so that ListA items are used in order \
-          with the learning material on ListC {title} '
+    template='Now, create a "method of loci"story with the ListA and ListC that you created {title} '
 )
 
 # Memory 
