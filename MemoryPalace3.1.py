7cfbe8c53bd2fedc7d8e781e6b651b07a736c42d
dillonfreed <42899695+dillonfreed@users.noreply.github.com>
2023-08-07 00:36:19 -0400

    Update MemoryPalace3.1.py

--- MemoryPalace3.1.py
+++ MemoryPalace3.1.py
@@ -1,7 +1,6 @@
 # Bring in deps
 import os 
 import streamlit as st 
-from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain 
 from langchain.memory import ConversationBufferMemory
