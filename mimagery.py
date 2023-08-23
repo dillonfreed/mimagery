912e519668b06b8c71648c8e230543dfbb42b279
dillonfreed <42899695+dillonfreed@users.noreply.github.com>
2023-08-23 19:31:45 -0400

    Update mimagery.py

--- mimagery.py
+++ mimagery.py
@@ -8,7 +8,7 @@ from langchain.chains import LLMChain, SequentialChain
 from langchain.memory import ConversationBufferMemory
 from langchain.utilities import WikipediaAPIWrapper 
 
-os.environ['OPENAI_API_KEY'] = apikey
+os.environ
 
 # App framework
 st.title('🦜Memorize 10 Times Fast')
