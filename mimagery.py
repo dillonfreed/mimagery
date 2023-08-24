d086aa3ebb4604dabc7e906c19e51857df9027cd
dillonfreed <42899695+dillonfreed@users.noreply.github.com>
2023-08-23 20:38:41 -0400

    Update mimagery.py

--- mimagery.py
+++ mimagery.py
@@ -1,6 +1,6 @@
 # Bring in deps
 import os 
-from st.secrets = ["api_key"] import apikey 
+from st.secrets import ["api_key"]
 
 import streamlit as st 
 from langchain.llms import OpenAI
@@ -9,7 +9,7 @@ from langchain.chains import LLMChain, SequentialChain
 from langchain.memory import ConversationBufferMemory
 from langchain.utilities import WikipediaAPIWrapper 
 
-os.environ['OPENAI_API_KEY'] = apikey
+os.environ['OPENAI_API_KEY'] = st.secrets["api_key"]
 
 # App framework
 st.title('🦜Memorize 10 Times Fast')
