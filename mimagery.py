6beeb22c5b3f4a73c534ebe70d3ed56a029a582d
dillonfreed <42899695+dillonfreed@users.noreply.github.com>
2023-08-27 14:06:56 -0400

    Update mimagery.py

--- mimagery.py
+++ mimagery.py
@@ -1,17 +1,15 @@
- import os
-#!pip install streamlit
-#!pip install langchain
-#!pip install openai
-os.environ['OPENAI_API_KEY'] = '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'
-
-
+import os
 import streamlit as st
 from langchain.llms import OpenAI
+
+pip install streamlit
+pip install langchain
+pip install openai
+os.environ['OPENAI_API_KEY'] = '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'
 
 st.title = ('Mimagery')
 prompt = st.text_input('Enter your prompt here')
 llm = OpenAI(temperature=0.9)
-
 
 if prompt:
  response = llm(prompt)
