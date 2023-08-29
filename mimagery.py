1b85c1bff46944fdfe3496c091a9871cdf5ee95e
dillonfreed <42899695+dillonfreed@users.noreply.github.com>
2023-08-29 15:36:48 -0400

    Update mimagery.py

--- mimagery.py
+++ mimagery.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 from langchain.llms import OpenAI
 
-os.environ['OPENAI_API_KEY'] = apikey
+os.environ['OPENAI_API_KEY'] = st.secrets.apikey
 
 st.title = ('Mimagery')
 prompt = st.text_input('Enter your prompt here')
